f647b089b9cea9e1b03840f28aec3089a303052d
Marina <34425664+mpmeleshko@users.noreply.github.com>
2018-02-14 17:07:09 +0200

    Update Comment.php

--- app/Comment.php
+++ app/Comment.php
@@ -5,32 +5,17 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
-
 {
-
 
     protected $guarded = [];
 
-
-
     public function article()
-
     {
-
         return $this->belongsTo(Article::class);
-
     }
-
 
     public function user()
-
     {
-
         return $this->belongsTo(User::class);
-
     }
-
-
-
-
 }
