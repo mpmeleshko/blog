cedebf5778b46f97c64889a7ea7c66987f334fbc
Marina <34425664+mpmeleshko@users.noreply.github.com>
2018-02-14 17:03:30 +0200

    Update FavoritesController.php

--- app/Http/Controllers/FavoritesController.php
+++ app/Http/Controllers/FavoritesController.php
@@ -11,36 +11,26 @@ class FavoritesController extends Controller
 
     public function __construct()
     {
-
         $this->middleware('auth');
-
     }
 
     public function store(Article $article, $id)
-
     {
-
         $article = Article::find($id);
         $article->favorite();
 
         return back();
-
     }
 
     public function delete($id)
-
     {
-
         $favorite = Article::find($id);
         $attributes = ['user_id' => auth()->id()];
-
 
         if ($favorite->favorites()->where($attributes)) {
             $favorite->favorites()->where($attributes)->delete();
         }
-
+        
         return back();
-
     }
-
 }
