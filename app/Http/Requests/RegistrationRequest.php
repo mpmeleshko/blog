574e5d4983ffaac45e90541f9588cd328fa233ac
Marina <34425664+mpmeleshko@users.noreply.github.com>
2018-02-19 12:40:07 +0200

    Update RegistrationRequest.php

--- app/Http/Requests/RegistrationRequest.php
+++ app/Http/Requests/RegistrationRequest.php
@@ -5,23 +5,15 @@ namespace App\Http\Requests;
 use Illuminate\Foundation\Http\FormRequest;
 
 class RegistrationRequest extends FormRequest
-
 {
-
-
     /**
      * Determine if the user is authorized to make this request.
      *
      * @return bool
      */
-
-
     public function authorize()
-
     {
-
         return true;
-
     }
 
 
@@ -30,18 +22,12 @@ class RegistrationRequest extends FormRequest
      *
      * @return array
      */
-
     public function rules()
-
     {
         return [
-
             'name' => 'required',
-
             'email' => 'required|email',
-
             'password' => 'required|confirmed'
         ];
     }
-
 }
